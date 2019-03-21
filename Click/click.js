1cc9cc737477405aa92ee3233a1043504427369b
Devin Gibbons <devingibbons@Devins-Air.lan1>
2019-03-20 21:37:39 -0500

    click game

---
+++ Click/click.js
@@ -0,0 +1,26 @@
+//set variable at 0 monsters
+var monstercount=0
+//click to add monsters
+function add(){
+//adding monsters every click
+    monstercount=monstercount+1
+//changing text to add number for monsters
+    document.getElementById('text').value=monstercount; 
+}
+
+//save amount of monster rawrs in localstorage
+function Save(){
+    localStorage.setItem("monsterrawrs",monstercount)
+}
+//getting saved item to return back to save point 
+function Load(){
+    monstercount=localStorage.getItem("monsterrawrs");
+    monstercount=parseInt(monstercount);
+    document.getElementById('text').value=monstercount;   
+}
+
+var restartButton
+function restart(){
+document.location.href="";
+
+}
